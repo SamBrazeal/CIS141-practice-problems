7d8dac5c11beb102bec5ec690d1df76ac3c0173d
SamBrazeal <SamuelBrazeal@student.olympic.edu>
2025-04-26 20:45:06 -0800

    3/4 questions separated

---
+++ Mod2-practice-problems/Question-3.py
@@ -0,0 +1,9 @@
+print("Question 3")
+import math
+
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = float(input("Enter the length of side c: "))
+s = (a + b + c) / 2
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+print(f"The area of the triangle is: {area:.2f}")
