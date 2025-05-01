25cdaa0e80aef4f52a6d77d790068597d2132f54
SamBrazeal <SamuelBrazeal@student.olympic.edu>
2025-04-30 18:22:49 -0800

    made the problem do the right thing and added comment of description

--- Mod3-practice-problems/Question-4.py
+++ Mod3-practice-problems/Question-4.py
@@ -1,9 +1,8 @@
+#4. Prompt the user for: a word, a first index, and a last index. Slice the word at the indexes provided by the user and print to the screen.
 sliceWord = input("Please input a word: ")
 sliceIndex1 = int(input("Please input a number: "))
 sliceIndex2 = int(input("Please input another number: "))
 
-wordSlice1 = sliceWord[:sliceIndex1]
-wordSlice2 = sliceWord[sliceIndex1:sliceIndex2]
-wordSlice3 = sliceWord[sliceIndex2:]
+WordSliced = sliceWord[sliceIndex1:sliceIndex2]
 
-print(f"Your word sliced: \n{wordSlice1} \n{wordSlice2} \n{wordSlice3}")
+print(f"Your word sliced: \n{WordSliced}")
