705d4f1ab55c8244c8c740ebed414e97135e4f1a
SamBrazeal <SamuelBrazeal@student.olympic.edu>
2025-04-30 17:25:36 -0800

    added description of problem as comment

--- Mod3-practice-problems/Question-3.py
+++ Mod3-practice-problems/Question-3.py
@@ -1,4 +1,5 @@
-Sentence = input("please enter a sentence: ")
+# 3. Prompt the user for a sentence and a word to try to find in that sentence.
+# Have the program print out whether the word was found in the sentence. (i.e. True or False) Sentence = input("please enter a sentence: ")
 Word_Search = input("what word would you like to find: ")
 
 print(Word_Search in Sentence)
