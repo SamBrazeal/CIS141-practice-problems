a91063d5cf890334994f07ea51a8dff4d712685d
SamBrazeal <SamuelBrazeal@student.olympic.edu>
2025-04-30 17:26:17 -0800

    added description of problem as comment

--- Mod3-practice-problems/Question-5.py
+++ Mod3-practice-problems/Question-5.py
@@ -1,3 +1,4 @@
+#5. Print 3 words with a | character (called a pipe) in between them. Use the appropriate keyword argument in print() to do so.
 Class1 = input("Enter 1st class name: ")
 Class2 = input("Enter 2nd class name: ")
 Class3 = input("Enter 3rd class name: ")
