a40186d9267bb9b7862f345c920ee66a7cca1f72
SamBrazeal <SamuelBrazeal@student.olympic.edu>
2025-04-30 17:20:39 -0800

    got rid of the eval for int since it comes out with the same output

--- Mod3-practice-problems/Question-2.py
+++ Mod3-practice-problems/Question-2.py
@@ -1,6 +1,6 @@
 Name = input("what is your name: ")
 Age = input("what is your age: ")
 
-Age_up = eval(Age) + 1
+Age_up = int(Age) + 1
 
 print("Hello, " + Name + "! You are " + Age + " years old. Next year, you will be " + str(Age_up) + " years old.")
